11f1fbd90c4d92ed732c413724e0053723dbf659
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-02-26 20:11:52 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.js");
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
